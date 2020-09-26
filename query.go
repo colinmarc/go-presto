269e753a2bb59dce74ba25c2cfd39602ee5e0eaf
SananGuliyev <sanan.quliyev@gmail.com>
2020-09-26 18:28:40 +0200

    fixed empty columns issue

--- query.go
+++ query.go
@@ -183,7 +183,7 @@ func (q *Query) fetchNext() error {
 	q.bufferedRows = result.Data
 	q.state = result.Stats.State
 
-	if q.columns == nil {
+	if q.columns == nil && len(result.Columns) > 0 {
 		q.columns = make([]string, len(result.Columns))
 		for i, col := range result.Columns {
 			q.columns[i] = col.Name
