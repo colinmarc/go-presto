95d5eac916c0cbd24d8b3cf81721f94dfa7fbcb1
SananGuliyev <sanan.quliyev@gmail.com>
2020-09-26 18:31:01 +0200

    use constants instead of hardcoded header values

--- presto.go
+++ presto.go
@@ -7,9 +7,10 @@ package presto
 const (
 	version = "0.1.0"
 
-	userHeader    = "X-Presto-User"
-	sourceHeader  = "X-Presto-Source"
-	catalogHeader = "X-Presto-Catalog"
-	schemaHeader  = "X-Presto-Schema"
-	userAgent     = "go-presto/" + version
+	userHeader      = "X-Presto-User"
+	sourceHeader    = "X-Presto-Source"
+	catalogHeader   = "X-Presto-Catalog"
+	schemaHeader    = "X-Presto-Schema"
+	userAgentHeader = "User-Agent"
+	userAgent       = "go-presto/" + version
 )
